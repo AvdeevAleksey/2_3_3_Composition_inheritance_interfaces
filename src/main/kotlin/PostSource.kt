f4ee780aacc0da627b05b7ba4b8892e08637a824
AvdeevAleksey <avdeev.hexer@yandex.ru>
2021-04-05 14:29:51 +0300

    Quick save)

--- src/main/kotlin/PostSource.kt
+++ src/main/kotlin/PostSource.kt
@@ -1,12 +1,28 @@
 class PostSource(
-    val type: String = ,
-    val platform: String = ,
-    val data: String = when(type) {
-
-                                  }
-    },
+    val type: String = "",
+    val platform: String = "",
+    data: String = "",
     val url: String = "http://"
-)
+) {
+    var data = data
+        get() = field
+        set(value) {
+            field = when(type){
+                "vk" -> when(value) {
+                    PostSourceData.profileActivity.string -> PostSourceData.profileActivity.string
+                    PostSourceData.profilePhoto.string -> PostSourceData.profilePhoto.string
+                    else -> ""
+                }
+                "widget" -> when(value) {
+                    PostSourceData.comments.string -> PostSourceData.comments.string
+                    PostSourceData.like.string -> PostSourceData.like.string
+                    PostSourceData.poll.string -> PostSourceData.poll.string
+                    else -> ""
+                }
+                else -> ""
+            }
+        }
+}
 
 enum class PostSourceType(val string: String) {
     vk("vk"),
